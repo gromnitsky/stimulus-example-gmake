2e03a52aecc6d932c4c3e2397d46e7d348daffd2
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-01-03 13:59:16 +0200

    mk: use common.mk

--- es6-modules.mk
+++ es6-modules.mk
@@ -1,10 +1,6 @@
-NODE_ENV ?= development
+NODE_ENV := development
 out := _out.$(basename $(lastword $(MAKEFILE_LIST))).$(NODE_ENV)
-mkdir = @mkdir -p $(dir $@)
-copy = cp $< $@
-
-compile.all :=
-compile:
+include common.mk
 
 
 
@@ -16,17 +12,6 @@ $(static.dest): $(out)/%: src/%
 	$(copy)
 
 compile.all += $(static.dest)
-
-
-
-vendor.src := stimulus/dist/stimulus.umd.js
-vendor.dest := $(addprefix $(out)/vendor/, $(vendor.src))
-
-$(out)/vendor/%: node_modules/%
-	$(mkdir)
-	$(copy)
-
-compile.all += $(vendor.dest)
 
 
 
